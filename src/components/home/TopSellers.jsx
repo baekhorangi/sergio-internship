1e239118ccf316527d5171dabfaee2047d1ff359
Sergio <deguzmansergei@gmail.com>
2023-02-26 08:17:07 -0700

    dynamic explore items + item component + cleanup

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
 import Skeleton from "../UI/Skeleton";
 
 const TopSellers = () => {
