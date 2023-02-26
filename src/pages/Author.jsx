7ecbf17d9905967fecc3ac51fa6fb48518fa20c8
Sergio <deguzmansergei@gmail.com>
2023-02-26 09:33:18 -0700

    Revert "Dynamic Author Page + dynamic routes"
    This reverts commit e2c195d424027ba54fbb1c80b5bb3197eb567822.

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,38 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link, useParams } from "react-router-dom";
-import axios from "axios";
-import Skeleton from "../components/UI/Skeleton";
+import { Link } from "react-router-dom";
+import AuthorImage from "../images/author_thumbnail.jpg";
 
 const Author = () => {
-  const { authorId } = useParams();
-  const [author, setAuthor] = useState(null);
-  const [followers, setFollowers] = useState(0);
-  const [followStatus, setFollowStatus] = useState("Follow");
-
-  const fetchAuthor = async () => {
-    const { data } = await axios.get(
-      `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${authorId}`
-    );
-    setFollowers(data.followers);
-    setAuthor(data);
-  };
-
-  useEffect(() => {
-    fetchAuthor();
-  }, []);
-
-  const handleFollow = () => {
-    if (followStatus === "Follow") {
-      setFollowStatus("Unfollow");
-      setFollowers(followers + 1);
-    } else {
-      setFollowStatus("Follow");
-      setFollowers(followers - 1);
-    }
-  };
-
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
@@ -43,7 +15,8 @@ const Author = () => {
           aria-label="section"
           className="text-light"
           data-bgimage="url(images/author_banner.jpg) top"
-          style={{ background: `url(${AuthorBanner}) top` }}></section>
+          style={{ background: `url(${AuthorBanner}) top` }}
+        ></section>
 
         <section aria-label="section">
           <div className="container">
@@ -52,65 +25,29 @@ const Author = () => {
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
                     <div className="profile_avatar">
-                      {author ? (
-                        <img src={author.authorImage} alt="" />
-                      ) : (
-                        <Skeleton
-                          width={"150px"}
-                          height={"150px"}
-                          borderRadius={"100%"}
-                        />
-                      )}
+                      <img src={AuthorImage} alt="" />
 
                       <i className="fa fa-check"></i>
                       <div className="profile_name">
                         <h4>
-                          {author ? (
-                            author.authorName
-                          ) : (
-                            <Skeleton width={"200px"} />
-                          )}
-                          <span className="profile_username">
-                            {author ? author.tag : <Skeleton width={"100px"} />}
-                          </span>
+                          Monica Lucas
+                          <span className="profile_username">@monicaaaa</span>
                           <span id="wallet" className="profile_wallet">
-                            {author ? (
-                              author.address
-                            ) : (
-                              <Skeleton width={"200px"} />
-                            )}
+                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
                           </span>
-                          {author && (
-                            <button
-                              id="btn_copy"
-                              title="Copy Text"
-                              onClick={() =>
-                                navigator.clipboard.writeText(author.address)
-                              }>
-                              Copy
-                            </button>
-                          )}
+                          <button id="btn_copy" title="Copy Text">
+                            Copy
+                          </button>
                         </h4>
                       </div>
                     </div>
                   </div>
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">
-                        {author ? (
-                          followers + " followers"
-                        ) : (
-                          <Skeleton width={"150px"} height={"40px"} />
-                        )}{" "}
-                      </div>
-                      {author && (
-                        <Link
-                          to="#"
-                          className="btn-main"
-                          onClick={handleFollow}>
-                          {followStatus}
-                        </Link>
-                      )}
+                      <div className="profile_follower">573 followers</div>
+                      <Link to="#" className="btn-main">
+                        Follow
+                      </Link>
                     </div>
                   </div>
                 </div>
@@ -118,7 +55,7 @@ const Author = () => {
 
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems author={author} />
+                  <AuthorItems />
                 </div>
               </div>
             </div>
