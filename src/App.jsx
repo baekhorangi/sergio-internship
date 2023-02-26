5693a9e355073d5c7294302166737df010d5ad38
Sergio <deguzmansergei@gmail.com>
2023-02-26 09:27:55 -0700

    Revert "Dynamic Author Page + dynamic routes"
    This reverts commit e2c195d424027ba54fbb1c80b5bb3197eb567822.

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/explore" element={<Explore />} />
-        <Route path="/author/:authorId" element={<Author />} />
-        <Route path="/item-details/:itemId" element={<ItemDetails />} />
+        <Route path="/author" element={<Author />} />
+        <Route path="/item-details" element={<ItemDetails />} />
       </Routes>
       <Footer />
     </Router>
